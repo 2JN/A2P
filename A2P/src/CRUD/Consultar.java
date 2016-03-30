d4fa153e9bd3a5c79b3e005e954da4bc553f2dee
2JN <jesquivelnavas@gmail.com>
2016-03-30 12:27:54 -0600

    Agrega opcion de consulta de inversionista por nombre

--- A2P/src/CRUD/Consultar.java
+++ A2P/src/CRUD/Consultar.java
@@ -1,10 +1,26 @@
 package CRUD;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class Consultar
-{
-	public Investigador consultarInversionista(String nombre,
-		String apellido, String direccion, String telefono)
-	{
+public class Consultar {
+    public ResultSet consultarInversionista(Connection cn, String nombre,
+        String apellido, String direccion, String telefono){
+        ResultSet st = null;
+        
+        if(!nombre.isEmpty() && apellido.isEmpty() && direccion.isEmpty() &&
+            telefono.isEmpty()){
+            
+            try{
+                PreparedStatement pst = cn.prepareStatement("SELECT " + nombre + 
+                    "FROM ");
+                ResultSet rs = pst.executeQuery();
+            } catch(SQLException ex){
 
-	}
+            } finally {
+                return st;
+            }
+        }
+    }
 }

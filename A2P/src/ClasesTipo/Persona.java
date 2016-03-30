37c0867184a1739264074ee2741dff9fa7b34066
chaqui <josue.chaqui@gmail.com>
2016-03-30 15:40:46 -0500

    agregando paquete ClasesTipo y clase persona

---
+++ A2P/src/ClasesTipo/Persona.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ClasesTipo;
+
+/**
+ *
+ * @author usuario
+ */
+public class Persona {
+    protected String nombre;
+    protected String apellido;
+    protected String direccion;
+    protected Integer telefono;
+    protected String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}

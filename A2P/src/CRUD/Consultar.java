37c268da92906c7b00df84d9dcddef303b7b6c91
2JN <jesquivelnavas@gmail.com>
2016-03-31 10:03:51 -0600

    Añadido modulo de prueba Consultar

--- A2P/src/CRUD/Consultar.java
+++ A2P/src/CRUD/Consultar.java
@@ -1,25 +1,51 @@
 package CRUD;
+import ClasesTipo.Inversionista;
+import ClasesTipo.Persona;
+import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Consultar {
-    public ResultSet consultarInversionista(Connection cn, String nombre,
-        String apellido, String direccion, String telefono){
-        ResultSet st = null;
+    public ArrayList<Inversionista> consultarInversionista(Connection con, 
+        String nombre, String apellido){
         
-        if(!nombre.isEmpty() && apellido.isEmpty() && direccion.isEmpty() &&
-            telefono.isEmpty()){
+        ArrayList<Inversionista> inv = new ArrayList<Inversionista>();
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        ResultSet rsInv = null;
+        
+        if(!nombre.isEmpty() && apellido.isEmpty()){
             
             try{
-                PreparedStatement pst = cn.prepareStatement("SELECT " + nombre + 
-                    "FROM ");
-                ResultSet rs = pst.executeQuery();
+                //Obtener los NIT de los inversionistas para hacer una sumatoria
+                //De las cantiades de inversión
+                pst = con.prepareStatement("SELECT * FROM Inversionista "
+                    + "WHERE nombre='" + nombre + "'");
+                rs = pst.executeQuery();
+                
+                while(rs.next()){
+                    int ttlInv = 0;
+                    
+                    rsInv = con.prepareStatement("SELECT cantidadInversion "
+                    +"FROM Inversion WHERE Inversionista_nit='"
+                    + rs.getInt(1) + "'")
+                    .executeQuery();
+                    
+                    while(rsInv.next()){
+                        ttlInv += rs.getInt(1);
+                    }
+                    
+                    Inversionista pr = new Inversionista(rs.getInt(1), ttlInv, 
+                        rs.getString(2), rs.getString(3), rs.getString(4), 
+                        rs.getInt(5), rs.getString(6));
+                    inv.add(pr);
+                }
             } catch(SQLException ex){
 
             } finally {
-                return st;
+                return inv;
             }
         }
     }
